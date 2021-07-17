0a56dcd8e813cbb47496f29c05f6ea53b5d91250
Anna <anna_agabekyan_anna@mail.ru>
2021-07-17 20:47:44 +0400

    feat: implementation of the modal window functionality: opening and closing

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+const morelElem = document.querySelector('.more')
+const modalElem = document.querySelector('.modal')
+
+const openModal = () => {
+    modalElem.classList.remove('hidden')
+}
+
+const closeModal = () => {
+    modalElem.classList.add('hidden')
+}
+
+morelElem.addEventListener('click', openModal)
+modalElem.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.classList.contains('overlay') || 
+        target.classList.contains('modal__close')) {
+            closeModal()
+    }        
+})
