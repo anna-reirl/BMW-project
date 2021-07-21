ae905d9a464357b2288e7f602d952f50793e48dc
Anna <anna_agabekyan_anna@mail.ru>
2021-07-21 14:33:00 +0400

    feat: work with forms

--- js/sendForm.js
+++ js/sendForm.js
@@ -22,13 +22,32 @@ const sendData = (data, callBack, falseCallBack) => {
 const dataTest = {
     name: 'Ivan',
     phone: '+9074958395'
-} 
+}
 
-sendData(JSON.stringify(dataTest), 
-(id) => {
-    alert('Ваша заявка №' + id + '! \nВ ближайшее время с вами свяжемся.')
-    console.log(id)
-}, 
-(err) => {
-    alert('К сожалению технические неполадки, попробуйте отправить заявку позже. ' + err)
-})
+
+const formsElems = document.querySelectorAll('.form')
+
+const formHandler = (form) => {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const data = {}
+
+        for (const {
+                name,
+                value
+            } of form.elements) {
+            if (name) {
+                data[name] = value
+            }
+        }
+        sendData(JSON.stringify(data),
+            (id) => {
+                alert('Ваша заявка №' + id + '! \nВ ближайшее время с вами свяжемся.')
+            },
+            (err) => {
+                alert('К сожалению технические неполадки, попробуйте отправить заявку позже. ' + err)
+            })
+    })
+}
+
+formsElems.forEach(formHandler)
